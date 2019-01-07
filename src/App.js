0ea627fdcb66be9eba9dd94a69504ee4a7ee6b14
Ryan Santos <ryansantos86@gmail.com>
2019-01-07 15:12:04 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
-  render() {
+
+  state = {
+    sequence: []
+  }
+
+  handleClick = (e) => {
+    const { value } = e.target, { sequence } = this.state;
+    sequence.push(+value);
+    this.setState({ sequence });
+  };
+
+  render = () => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div>
+        <button onClick={this.handleClick} value={0}>0</button>
+        <button onClick={this.handleClick} value={1}>1</button>
+        <button onClick={this.handleClick} value={2}>2</button>
+        <button onClick={this.handleClick} value={3}>3</button>
+        </div>
+        <div>
+        <p>{this.state.sequence}</p>
+        </div>
       </div>
     );
-  }
+  };
 }
 
 export default App;

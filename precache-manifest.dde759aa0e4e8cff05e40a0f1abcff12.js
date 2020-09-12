e43fd01e89c222dbac4d32e883e9c1132bd328ea
Ryan Santos <ryansantos86@gmail.com>
2020-09-11 17:42:23 -0700

    Updates

---
+++ precache-manifest.dde759aa0e4e8cff05e40a0f1abcff12.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef4273e7e4af68fcd9ee08e3c51e5ef7",
+    "url": "/simon/index.html"
+  },
+  {
+    "revision": "3945bc6bdc6ed6de5cf9",
+    "url": "/simon/static/css/main.82cce90e.chunk.css"
+  },
+  {
+    "revision": "acacde5606b4d20997ef",
+    "url": "/simon/static/js/2.42363c45.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/simon/static/js/2.42363c45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3945bc6bdc6ed6de5cf9",
+    "url": "/simon/static/js/main.f421fbbc.chunk.js"
+  },
+  {
+    "revision": "1e6ed640293b164133cf",
+    "url": "/simon/static/js/runtime-main.62a46b23.js"
+  },
+  {
+    "revision": "ce1af83f9490f49701b90d2c8b40a892",
+    "url": "/simon/static/media/ghlogo.ce1af83f.svg"
+  },
+  {
+    "revision": "37e5b1b199af16cf730ed8dceaa3abd2",
+    "url": "/simon/static/media/simonSound1.37e5b1b1.mp3"
+  },
+  {
+    "revision": "3099af8a014befe65871eaf617153b98",
+    "url": "/simon/static/media/simonSound2.3099af8a.mp3"
+  },
+  {
+    "revision": "691b87b64b8ad579d9d11ae276f318ac",
+    "url": "/simon/static/media/simonSound3.691b87b6.mp3"
+  },
+  {
+    "revision": "309847df9d56db45b663017511926bad",
+    "url": "/simon/static/media/simonSound4.309847df.mp3"
+  }
+]);
